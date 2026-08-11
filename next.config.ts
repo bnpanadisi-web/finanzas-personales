b8543843c5d2355806963d2bc8a649a408676d0e
bnpanadisi-web <pana.tech25@gmail.com>
2026-08-10 23:02:30 -0300

    Fix scripts in package.json for vercel

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Evita que TypeScript se quede colgado en la fase de verificación en Vercel
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Evita que ESLint bloquee la compilación
     ignoreDuringBuilds: true,
   },
 };
